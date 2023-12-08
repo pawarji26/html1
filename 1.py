8ef23bb09c923391ae43b732cf2e9e45b95867e6
pawarji26 <140817999+pawarji26@users.noreply.github.com>
2023-12-08 17:16:59 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+# Variables and basic operations
+x = 90
+y = 30
+result = x + y
+print(result)  # Output: 13
+
+
+
---
+++ 2.py
@@ -0,0 +1,4 @@
+# String concatenation
+name = "John"
+greeting = "Hello, " + name + "!"
+print(greeting)  # Output: Hello, John!
